1db61af3256f2049c0f0c04ed9f6847fede33a11
elpreben <preben.nygard@gmail.com>
2025-07-27 16:21:50 +0200

    Update kontakt.js

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -36,7 +36,59 @@ export default function Kontakt() {
               Fyll ut skjema, og vi vil kontakte deg innen 48 timer for råd og veiledning samtale.
               <br />
               <span className="font-semibold">
-                (Estimert tid: 10-15 min per samtale, Kr 50,- inkl. MVA)
+                (Estimert avsatt tid: 10–15 minutter per samtale, Kr 50,- inkl. MVA)
               </span>
             </p>
-            <form onSubmit={handleSubmit} className="spa
+            <form onSubmit={handleSubmit} className="space-y-2">
+              <input
+                name="navn"
+                type="text"
+                className="w-full border p-2 rounded text-sm"
+                placeholder="Fullt navn"
+                required
+              />
+              <input
+                name="telefon"
+                type="text"
+                className="w-full border p-2 rounded text-sm"
+                placeholder="Telefonnummer"
+                required
+              />
+              <input
+                name="adresse"
+                type="text"
+                className="w-full border p-2 rounded text-sm"
+                placeholder="Adresse"
+                required
+              />
+              <input
+                name="postnummer"
+                type="text"
+                className="w-full border p-2 rounded text-sm"
+                placeholder="Postnummer"
+                required
+              />
+              <input
+                name="email"
+                type="email"
+                className="w-full border p-2 rounded text-sm"
+                placeholder="E-postadresse"
+                required
+              />
+              <button
+                type="submit"
+                className="w-full bg-gradient-to-r from-blue-600 to-green-500 text-white font-bold py-2 rounded-lg text-sm"
+              >
+                {loading ? 'Sender...' : 'SEND MELDING'}
+              </button>
+            </form>
+          </>
+        ) : (
+          <p className="text-center font-semibold text-md">
+            Takk! Vi har mottatt din forespørsel. Vi tar kontakt innen 48 timer.
+          </p>
+        )}
+      </div>
+    </Layout>
+  );
+}
